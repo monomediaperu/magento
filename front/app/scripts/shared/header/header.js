81b7a4a5ba2872492d1c734415a8b2246a1ceaaa
Lou Alcalá <lou.alcala.dev@gmail.com>
2017-09-30 14:30:26 -0500

    fix mobile menu

--- front/app/scripts/shared/header/header.js
+++ front/app/scripts/shared/header/header.js
@@ -10,7 +10,19 @@
             replace: true,
             restrict: 'E',
             scope: {},
-            templateUrl: 'scripts/shared/header/header.html'
+            templateUrl: 'scripts/shared/header/header.html',
+            controller: appHeaderCtrl,
+            controllerAs: 'vm'
         };
+        
+        function appHeaderCtrl() {
+            var vm = this;
+
+            vm.collapseModal = collapseModal;
+            
+            function collapseModal() {
+                angular.element('.navbar-collapse').collapse('hide');;
+            }
+        }
     }
 }());
